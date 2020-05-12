0c03aa1eaf3c529fd6dcae88301875af2fab7a20
deng.bozhi <deng.bozhi@immomo.com>
2020-05-12 10:40:55 +0800

    rebuilding site 2020年 5月12日 星期二 10时40分55秒 CST

---
+++ js/baiduAnalytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?4646b61c8b5071a27dbdac7ac69ba9da";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
